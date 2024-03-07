5f5ec9ee1d4bae2b4e908fac7de0e955956036a2
Abhishek mishra <70240207+abhishek2914@users.noreply.github.com>
2024-03-07 14:57:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,26 +8,27 @@ st.write("""
         """
          )
 
-model_path = r'/Users/abhishek/Desktop/malaria/model_vgg19.h5'
+model_path = "/Users/abhishek/Desktop/malaria/model_vgg19.h5"
 
 upload_file = st.sidebar.file_uploader("upload cell images", type = "png")
 
 Generate_pred = st.sidebar.button("Predict")
 
-model= tf.keras.models.load_model(model_path)
+model = tf.keras.models.load_model(model_path)
 
 def import_n_pred(image_data,model):
-    size = (150,150)
+    size = (224,224)
     image = ImageOps.fit(image_data,size,Image.ANTIALIAS)
     img = np.asarray(image)
     reshape = img[np.newaxis,...]
+    pred = model.predict(reshape)
     return pred
 
 if Generate_pred:
     image =Image.open(upload_file)
     with st.expander('Cell Image', expanded= True):
         st.image(image,use_column_width=True)
-    pred=import_n_pred(image,model)
+    pred = import_n_pred(image,model)
     labels = ["MALARIA POSITIVE","MALARIA NEGATIVE"]
     st.title("Result:{}".format(labels[np.argmax(pred)]))
 
